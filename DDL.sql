68152bc0294e0a9c47aade87f698cbfb7fd5cc83
Valeria Gonzalez <gonzalezguerrerovm@gmail.com>
2022-11-30 10:19:55 +0100

    Agrego observation a tabla inspection y mas datos, correciones de ortografía

--- DDL.sql
+++ DDL.sql
@@ -1,10 +1,10 @@
 
---Creamos el espacio de trabajo 
+--Creación del espacio de trabajo 
 
 create schema practica authorization plopwnoj;
 
 
---Creación de las tablas : CORPORATEGROUP, BRAND, MODEL
+--Creación de las tablas CORPORATEGROUP, BRAND, MODEL
 
 
 create table practica.corporategroup (
@@ -48,7 +48,7 @@ references practica.brand (idbrand);
 
 
 
---Inserción de datos en estas dos tablas
+--Inserción de datos en estas tres tablas
 
 
 insert into practica.corporategroup (idcorp, corp_name) values ('001', 'Volkswagen Group');
@@ -73,7 +73,7 @@ insert into practica.model (idmodel, idbrand, model_name) values ('008', '004', 
 
 
 
---Creacion de tablas auxiliares: INSURER, CURRENCY y COLOR
+--Creación de tablas auxiliares: INSURER, CURRENCY y COLOR
 
 
 create table practica.insurer (
@@ -106,7 +106,7 @@ add constraint color_PK primary key (idcolor);
 
 
 
---Inserción de datos en estas dos tablas 
+--Inserción de datos en estas tres tablas auxiliares
 
 insert into practica.insurer (idinsurer, insurer_name) values ('001', 'Maphre');
 insert into practica.insurer (idinsurer, insurer_name) values ('002', 'Allianz');
@@ -125,7 +125,7 @@ insert into practica.color (idcolor, color_name) values ('004', 'Negro');
 
 
 
---Creacion de tabla CAR con sus primary keys y foreign keys
+--Creación de tabla CAR con sus primary keys y foreign keys
 
 
 create table practica.car (
@@ -159,11 +159,11 @@ add constraint color_FK foreign key (idcolor)
 references practica.color (idcolor);
 
 
---Insercion de datos en CAR
+--Inserción de datos en CAR
 
-insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('001', '001', '002', '001', '9823KFH', 23000, 1092839, '2020-03-12');
-insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('002', '007', '001', '004', '1824KFL', 34000, 2092431, '2020-09-20');
-insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('003', '003', '004', '003', '0821AFH', 50000, 9012833, '2021-01-10');
+insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('001', '001', '002', '001', '9823QFH', 21000, 1092839, '2020-03-12');
+insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('002', '007', '001', '004', '1824AML', 30000, 2092431, '2020-09-20');
+insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('003', '003', '004', '003', '0821AFP', 45000, 9012833, '2021-01-10');
 insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date, observation) values ('004', '005', '001', '003', '3825KFJ', 21000, 6092830, '2022-04-26', 'Fallos en las luces, requiere revisión');
 insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('005', '002', '003', '002', '4813KGH', 15000, 2012830, '2022-09-05');
 insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, mileage, insurance_num, purchase_date) values ('006', '008', '004', '001', '7813BGS', 12000, 8032240, '2022-11-09');
@@ -171,20 +171,20 @@ insert into practica.car (idcar, idmodel, idinsurer, idcolor, registration_num, 
 
 
 
---Creacion de tabla INSPECTION con sus primary keys y foreign keys
+--Creación de tabla INSPECTION con sus primary keys y foreign keys
 
 create table practica.inspection (
 	idcar varchar(10) not null, --pf, fk
 	idinspection varchar(10) not null, --pk
-	kms integer not null,
+	mileage integer not null,
 	inspect_date date not null,
-	amount integer not null,
-	idcurrency varchar(10) not null
+	amount decimal not null,
+	idcurrency varchar(10) not null,
+	observations varchar(256) null default 'Sin observaciones' 
 );
 
 alter table practica.inspection
 add constraint idinspection_PK primary key (idcar, idinspection);
-
 
 alter table practica.inspection
 add constraint idcar_FK foreign key (idcar)
@@ -198,9 +198,11 @@ references practica.currency (idcurrency);
 --Inserción de datos en INSPECTION
 
 
-insert into practica.inspection (idcar, idinspection, kms, inspect_date, amount, idcurrency) values ('001', '001', 21000, '2021-03-10', 550, '002');
-insert into practica.inspection (idcar, idinspection, kms, inspect_date, amount, idcurrency) values ('002', '001', 30000, '2021-09-16', 780, '002');
-insert into practica.inspection (idcar, idinspection, kms, inspect_date, amount, idcurrency) values ('003', '001', 45000, '2022-01-08', 870, '002');
+insert into practica.inspection (idcar, idinspection, mileage, inspect_date, amount, idcurrency) values ('001', '001', 21000, '2021-03-10', 149.79, '002');
+insert into practica.inspection (idcar, idinspection, mileage, inspect_date, amount, idcurrency) values ('002', '001', 30000, '2021-09-16', 179.99, '002');
+insert into practica.inspection (idcar, idinspection, mileage, inspect_date, amount, idcurrency) values ('003', '001', 45000, '2022-01-08', 169.99, '001');
+insert into practica.inspection (idcar, idinspection, mileage, inspect_date, amount, idcurrency) values ('001', '002', 32000, '2022-03-19', 219.89, '002');
+insert into practica.inspection (idcar, idinspection, mileage, inspect_date, amount, idcurrency) values ('002', '002', 40000, '2022-09-28', 279.99, '002');
 
 
 
--- DML.sql
+++ DML.sql
@@ -1,13 +1,16 @@
 
--- Nombre modelo, marca y grupo de coches (los nombre de todos)
--- Fecha de compra
--- Matricula
--- Nombre del color del coche
--- Total kilómetros
--- Nombre empresa que esta asegurado el coche
--- Numero de póliza
+--Consulta DML para obtener:
+
+	-- Nombre modelo, marca y grupo de coches (los nombre de todos)
+	-- Fecha de compra
+	-- Matricula
+	-- Nombre del color del coche
+	-- Total kilómetros
+	-- Nombre empresa que esta asegurado el coche
+	-- Numero de póliza
 
 
 select a.model_name , b.brand_name , c.corp_name , d.purchase_date , d.registration_num , e.color_name , d.mileage , f.insurer_name , d.insurance_num  
 from practica.model a, practica.brand b, practica.corporategroup c, practica.car d, practica.color e, practica.insurer f
-where a.idmodel = d.idmodel and a.idbrand = b.idbrand and b.idcorp = c.idcorp and d.idcolor = e.idcolor and d.idinsurer = f.idinsurer;
+where a.idmodel = d.idmodel and a.idbrand = b.idbrand and b.idcorp = c.idcorp and d.idcolor = e.idcolor and d.idinsurer = f.idinsurer
+order by d.purchase_date asc
